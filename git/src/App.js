d8722c3c2f6dedc3516f040ee62935ebceced5de
NahlaElsayed <110737762+NahlaElsayed@users.noreply.github.com>
2022-10-03 23:29:53 +0200

    Add files via upload

---
+++ git/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Lap from "./lapsol";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Lap> </Lap>{" "}
+    </div>
+  );
+}
+
+export default App;
